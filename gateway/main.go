2a90706c655662cbdbf45843fbf1aa00c49e6b76
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2018-11-06 09:00:10 +0100

    all: init

---
+++ gateway/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+
+	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	gw "github.com/cernbox/cernboxapis/gen/proto/go/cernbox/about/v1"
+)
+
+var (
+	grpcServer = flag.String("grpc_server", "localhost:9901", "about grpc server")
+)
+
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := gw.RegisterAboutHandlerFromEndpoint(ctx, mux, *grpcServer, opts)
+	if err != nil {
+		return err
+	}
+
+	return http.ListenAndServe(":8081", mux)
+}
+
+func main() {
+	flag.Parse()
+	defer glog.Flush()
+
+	if err := run(); err != nil {
+		glog.Fatal(err)
+	}
+}
---
+++ grpcserver/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/cernbox/cernboxapis/gen/proto/go/cernbox/about/v1"
+	"github.com/cernbox/cernboxapis/gen/proto/go/cernbox/rpc"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	var opts []grpc.ServerOption
+	s := grpc.NewServer(opts...)
+	srv := &server{}
+	aboutv1pb.RegisterAboutServer(s, srv)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", 9901))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s.Serve(lis)
+}
+
+type server struct{}
+
+func (s *server) ListMembers(ctx context.Context, req *aboutv1pb.ListMembersRequest) (*aboutv1pb.ListMembersResponse, error) {
+	status := &rpcpb.Status{
+		Code: rpcpb.Code_CODE_OK,
+	}
+
+	res := &aboutv1pb.ListMembersResponse{
+		Members: getCernboxMembers(),
+		Status:  status,
+	}
+
+	return res, nil
+}
+
+func (s *server) GetDocumentation(ctx context.Context, req *aboutv1pb.GetDocumentationRequest) (*aboutv1pb.GetDocumentationResponse, error) {
+	status := &rpcpb.Status{
+		Code: rpcpb.Code_CODE_OK,
+	}
+
+	res := &aboutv1pb.GetDocumentationResponse{
+		Status:        status,
+		Documentation: getDoc(),
+	}
+
+	return res, nil
+
+}
+
+func getCernboxMembers() []*aboutv1pb.Member {
+	people := map[string]string{
+		"hugo.gonzalez.labrador@cern.ch": "Hugo Gonzalez Labrador",
+	}
+
+	members := []*aboutv1pb.Member{}
+	for mail, name := range people {
+		m := &aboutv1pb.Member{
+			Email:       mail,
+			DisplayName: name,
+		}
+		members = append(members, m)
+	}
+
+	return members
+}
+
+func getDoc() *aboutv1pb.Documentation {
+	return &aboutv1pb.Documentation{
+		GithubUrl:  "https://github.com/cernbox",
+		ServiceUrl: "https://cernbox.cern.ch",
+	}
+}
